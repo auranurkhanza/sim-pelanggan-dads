1184ff6b53150fba790f34f27bed98429d0a763b
auranurkhanza <auranurkhanza01@gmail.com>
2026-09-17 13:28:40 +0700

    Create dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard - SIM Pelanggan DADS</title>
+</head>
+<body>
+    <h1>Selamat Datang di Dashboard SIM Pelanggan DADS!</h1>
+    <p>Anda berhasil login.</p>
+</body>
+</html>
